cc59513559dae71426f0feae22ea3d5ea5aa9980
Jean Carlos Meninno <jean-carlos.meninno@xing.com>
2014-11-29 13:54:53 +0100

    Added command line interface

---
+++ index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+global.__base = __dirname + '/src/';
+
+// Commands
+var cli = require('commander');
+
+cli
+  .version('0.0.1')
+  .option('-p, --port <port>'            , 'Change static server port [8000]', 8000)
+  .option('-P, --livereload-port <port>' , 'Change livereload port [35729]', 35729)
+  .option('-d, --directory <path>'       , 'Change the default directory for serving files [.]', __dirname)
+  .parse(process.argv);
+
+
+
+// Start Asset server
+var AssetServer = require(__base + 'assets_server');
+var assetServer = new AssetServer({
+  port: cli.port,
+  directory: cli.directory,
+  livereloadPort: cli.livereloadPort
+});
+
+assetServer.start();
+
+
+// Start Livereload server
+var LivereloadServer = require(__base + 'livereload_server');
+var livereloadServer = new LivereloadServer({
+  port: cli.livereloadPort
+});
+
+livereloadServer.start();
+
+
+// Start Monitor
+var Monitor = require(__base + 'monitor');
+var monitor = new Monitor({
+  directory: cli.directory,
+  livereloadPort: cli.livereloadPort
+})
+
+monitor.start();
